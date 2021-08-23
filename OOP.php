7ecf4a631e9e302004aec4f41c3064d4ac5274fe
Voduykhanhit <voduykhanhit@gmail.com>
2021-08-23 18:27:00 +0700

    Change file OOP

--- OOP.php
+++ OOP.php
@@ -18,8 +18,32 @@
     }
     class Student extends Teacher{
     }
+    // Abstact class
+    abstract class Data{
+        public function CongHieu(){
+
+        }
+        abstract public function DuyKhanh();
+    }
+    class Dt extends Data{
+        public function DuyKhanh(){
+            echo "Đây là function kế thừa từ abstact class";
+        }
+    }
+    // Interface
+    interface Model{
+        public function Setname();
+        const Connguoi = "OK";
+    }
+    class MD implements Model{
+        public function Setname(){
+            echo "Đây là class kế thừa từ interface";
+        }
+    }
     echo Teacher::getAge("18TH1");
     $st = new Student;
     $st->KhoaHoc('DuyKhanh','18TH1','Công nghệ web');
+    $itf = new MD;
+   echo $ift->Setname();
 
 ?>
